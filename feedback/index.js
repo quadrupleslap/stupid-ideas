627c2ecd2a74628ae522fe7d000da0376a7567b6
Ram Kaniyur <quadrupleslap@gmail.com>
2018-11-13 21:16:27 +1100

    Choose an output device.

--- feedback/index.js
+++ feedback/index.js
@@ -1,16 +1,44 @@
 (async function () {
 
 try {
-    document.body.textContent = 'Loading…';
-    let stream = await navigator.mediaDevices.getUserMedia({audio: true});
     let audio = document.createElement('audio');
+    let outputs = document.createElement('select');
+
+    stat.textContent = 'Loading…';
+
+    // Load the list of output devices.
+
+    let devices = await navigator.mediaDevices.enumerateDevices();
+
+    outputs.required = true;
+    for (let device of devices) {
+        if (device.kind != 'audiooutput') continue;
+        let option = document.createElement('option');
+        option.value = device.deviceId;
+        option.text = device.label;
+        outputs.add(option);
+    }
+
+    outputs.addEventListener('change', e => {
+        let id = outputs.options[outputs.selectedIndex].value;
+        audio.setSinkId(id);
+    });
+
+    document.body.appendChild(outputs);
+
+    // Load the input stream.
+
+    let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    // Play the input stream to the selected output device.
+
     audio.srcObject = stream;
     audio.onloadedmetadata = e => {
         audio.play();
-        document.body.textContent = 'Looping!';
+        stat.textContent = 'Looping!';
     };
 } catch (e) {
-    document.body.textContent = 'Failed.'
+    stat.textContent = 'Failed.';
     console.error(e);
 }
 
